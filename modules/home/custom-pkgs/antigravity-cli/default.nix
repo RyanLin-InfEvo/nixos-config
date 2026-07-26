411b5d3609d3fe27e77c48c0a3a9ca48febf294c
RyanLin-InfEvo <ryanarduino0410@gmail.com>
2026-07-26 20:35:45 +0800

    feat: update antigravity-cli to version 1.1.7 with new source URL and hash

--- modules/home/custom-pkgs/antigravity-cli/default.nix
+++ modules/home/custom-pkgs/antigravity-cli/default.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "antigravity-cli";
-  version = "1.1.5-5958982624477184";
+  version = "1.1.7-5951805767680000";
 
   src = pkgs.fetchurl {
-    url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.5-5958982624477184/linux-x64/cli_linux_x64.tar.gz";
-    hash = "sha512-kGv/Wcc77WMCdPZ+/Hfj+iBk/hJvTXUhx+WNxnc9jR886GiPEXj372V3Zyj3gqjrwhEYlhfPu0Ln30j1YUrR9Q==";
+    url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.7-5951805767680000/linux-x64/cli_linux_x64.tar.gz";
+    hash = "sha512-cg1af/JWql3WcSUTzV62/gMc+edSOjO8vad1USDO1Tu2T/mFtALOBo5YleD/s0jCYyVFA5od3m2q1ZHxZNWFLw==";
   };
 
   nativeBuildInputs = [ pkgs.autoPatchelfHook ];
